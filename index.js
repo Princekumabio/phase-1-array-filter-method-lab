7afd0b1b86f0933ae2fa694cf26816a6204dd4b0
kumabio <kumabio.prince@student.moringaschool.com>
2022-05-19 06:09:11 +0000

    Partially completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(function (driverName) {
+       return driverName.toLowerCase() === name.toLowerCase()
+ } )
+     };
+
+Console.log(driverName);
+
+function fuzzyMatch(drivers, letter) {
+   let filteredNames = drivers.filter(function(word) {
+         return word.charAt(index(0)) === letter;
+      });
+      return filteredNames;
+  };
+
+Console.log(filteredNames);
